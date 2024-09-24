65f79f23455f43bb011be333d66e0c982b9da70a
Sufiyan <sufiyanmemon687@gmail.com>
2024-09-24 15:26:50 +0530

    renamed folders and added services section main page

---
+++ src/pages/Home/Main/Main.tsx
@@ -0,0 +1,15 @@
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <div className="main-wrapper">
+      <div className="main">
+          <h1>Elevating brand value<br/> with design strategy</h1>
+          <p>Beyond Web's full-service design team. With our <br/> strategy-first approach we amplify brands into a force.</p>
+          <button className="see-how-btn">See how</button>
+      </div>
+    </div>
+  )
+}
+
+export default Main
