0f073f95399264a01f9e7ff0510e4ee4c1f7c31a
Sufiyan <sufiyanmemon687@gmail.com>
2024-11-24 14:14:33 +0530

    added review section

--- src/pages/Home/Reviews/Reviews.tsx
+++ src/pages/Home/Reviews/Reviews.tsx
@@ -1,15 +1,13 @@
 import Slider, { Settings } from "react-slick";
 import "./Reviews.css";
-
+import ReactStars from "react-stars";
+import { useEffect, useRef } from "react";
 
 const CustomPrevArrow = (props: any): React.JSX.Element => {
-    const { onClick } = props;
-    return (
-      <button 
-        className="custom-arrow custom-prev" 
-        onClick={onClick}
-      >
-        <svg
+  const { onClick } = props;
+  return (
+    <button className="custom-arrow custom-prev" onClick={onClick}>
+      <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
@@ -23,18 +21,15 @@ const CustomPrevArrow = (props: any): React.JSX.Element => {
         <line x1="15" y1="18" x2="9" y2="12"></line>
         <line x1="15" y1="6" x2="9" y2="12"></line>
       </svg>
-      </button>
-    );
-  };
-  
-  const CustomNextArrow = (props: any): React.JSX.Element => {
-    const { onClick } = props;
-    return (
-      <button 
-        className="custom-arrow custom-next" 
-        onClick={onClick}
-      >
-        <svg
+    </button>
+  );
+};
+
+const CustomNextArrow = (props: any): React.JSX.Element => {
+  const { onClick } = props;
+  return (
+    <button className="custom-arrow custom-next" onClick={onClick}>
+      <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
@@ -48,52 +43,77 @@ const CustomPrevArrow = (props: any): React.JSX.Element => {
         <line x1="9" y1="18" x2="15" y2="12"></line>
         <line x1="9" y1="6" x2="15" y2="12"></line>
       </svg>
-      </button>
-    );
-  };
+    </button>
+  );
+};
 
 const settings: Settings = {
   infinite: true,
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: false,
+  autoplay: true,
   arrows: true,
   dots: false,
-  prevArrow: <CustomPrevArrow/>,
-  nextArrow: <CustomNextArrow/>
+  prevArrow: <CustomPrevArrow />,
+  nextArrow: <CustomNextArrow />,
 };
 
+const starsSettings = {
+  edit: false,
+  size: 40,
+  count: 5,
+  color2: "orange",
+  value: 5,
+};
 
 const sliderData = [
   {
-    review:
-      "Had an awesome experience working with Dino, he made a fantastic site for us and was a pleasure to work with from start to finish. He also gave great support after the site was done, you know when most vendors disappear...He is an amazingly talented guy, I highly recommend him. You will not be disappointed!",
-    author: "Andrew Ramirez",
+    review: "Great service and support! Highly recommended.",
+    author: "John Doe",
     link: "#",
   },
   {
-    review:
-      "HIRE THEM!!! I had the pleasure of working with RSNL Creative to create my photography website, and I couldn't be happier with the results! From start to finish, they were incredibly professional, responsive, and creative.The team at RSNL took the time to understand my vision for the my website and turned it into a stunning online portfolio that showcases my work beautifully. Their attention to detail and design expertise really shine through in every aspect of the site.Not only did they deliver a visually appealing website, but they also ensured it was fully functional and user-friendly, making it easy for...",
-    author: "Cindi Sager",
+    review: "Excellent experience! The team was professional and delivered on time.",
+    author: "Jane Smith",
     link: "#",
   },
   {
-    review:
-      "Had an awesome experience working with Dino, he made a fantastic site for us and was a pleasure to work with from start to finish. He also gave great support after the site was done, you know when most vendors disappear...He is an amazingly talented guy, I highly recommend him. You will not be disappointed!",
-    author: "Andrew Ramirez",
-    link: "#",
-  },
-  {
-    review: "Super helpful, super responsive, super affordable",
-    author: "Hi Bear",
+    review: "Very satisfied with the results. Will definitely work with them again.",
+    author: "Michael Brown",
     link: "#",
   },
 ];
 
 const Reviews = () => {
+
+  // Type the ref to indicate it will reference an HTMLDivElement
+  const reviewsWrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+      const handleScroll = () => {
+          if (reviewsWrapperRef.current) {
+              const scrollY = window.scrollY;
+              console.log("y",scrollY)
+              // Change the background color based on scroll position
+              if (scrollY < 2850) {
+                reviewsWrapperRef.current.style.backgroundColor = "white";
+              } else {
+                reviewsWrapperRef.current.style.backgroundColor = "black";
+              }
+          }
+      };
+      
+      window.addEventListener("scroll", handleScroll);
+      
+      // Cleanup the event listener on component unmount
+      return () => {
+          window.removeEventListener("scroll", handleScroll);
+      };
+  }, []);
+
   return (
-    <div className="reviews-wrapper">
+    <div className="reviews-wrapper" ref={reviewsWrapperRef}>
       <div className="reviews-title-wrapper">
         <h1>Reviews</h1>
       </div>
@@ -102,7 +122,9 @@ const Reviews = () => {
           <div className="reviews-subtitle-wrapper">
             <p>Proven results are waiting for you.</p>
           </div>
-          <div className="reviews-stars-wrapper">Stars</div>
+          <div className="reviews-stars-wrapper">
+            <ReactStars {...starsSettings} />
+          </div>
         </div>
         <div className="reviews-right-wrapper">
           <div className="reviews-slider-wrapper">
---
+++ src/pages/Home/StartProject/StartProject.tsx
@@ -0,0 +1,9 @@
+import './StartProject.css'
+
+const StartProject = () => {
+  return (
+    <div>StartProject</div>
+  )
+}
+
+export default StartProject
