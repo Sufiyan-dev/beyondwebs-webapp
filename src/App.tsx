fb6fb9f3f3c4419edf2daab527e6e926bd856d71
Sufiyan <sufiyanmemon687@gmail.com>
2024-12-03 14:32:45 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
+import Projects from './pages/Projects/Projects'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      {/* Navbar stays at the top of every page */}
+      <Navbar />
+
+      {/* Main content changes based on the route */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </main>
+
+      {/* Footer stays at the bottom of every page */}
+      <Footer />
     </>
   )
 }
