e80299733e193046535c56746cc7129a91266848
Sufiyan <sufiyanmemon687@gmail.com>
2024-12-24 12:40:34 +0530

    added links to pages

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import "./Navbar.css"
+import { useLocation, useNavigate } from 'react-router-dom';
+import CustomNavLink from '../CustomNavLink/CustomNavLink';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [scrolled, setScrolled] = useState(false);
   const [shadowColor, setShadowColor] = useState('rgba(255, 255, 255, 0.5)'); // Default to a light shadow.
 
@@ -33,34 +37,56 @@ const Navbar = () => {
     };
   }, []);
 
-  const scrollToServiceSection = () => {
-    const serviceSection = document.getElementById("service-section");
-    if (serviceSection) {
-      serviceSection.scrollIntoView({ behavior: "smooth" });
+
+
+  const handleServicesClick = () => {
+    if (location.pathname === "/") {
+      // Scroll directly if already on the main page
+      document.getElementById("service-section")?.scrollIntoView({ behavior: "smooth" });
+    } else {
+      // Redirect and scroll on the main page
+      navigate("/");
+
+      setTimeout(() => {
+        document.getElementById("service-section")?.scrollIntoView({ behavior: "smooth" });
+      }, 300); // Allow time for navigation
     }
   };
 
 
   return (
-  <div className={`navbar ${scrolled ? 'scrolled' : ''}`} 
-  style={{
-    boxShadow: scrolled ? `0 0px 30px ${shadowColor}` : "none",
-  }}>
-    <div className="navbar-wrapper">
-      <div className="navbar-left">
-        <h1 className="navbar-logo">BeyondWebs</h1>
-      </div>
-      <div className="navbar-right">
-        <ul className="navbar-menu">
-          <li className="navbar-item underline-effect" onClick={scrollToServiceSection}>Services</li>
-          <li className="navbar-item">Projects</li>
-          <li className="navbar-item">About</li>
-          <li className="navbar-item navbar-item-highlight">Start Your Project</li>
-        </ul>
+    <div
+      className={`navbar ${scrolled ? "scrolled" : ""}`}
+      style={{
+        boxShadow: scrolled ? `0 0px 30px ${shadowColor}` : "none",
+      }}
+    >
+      <div className="navbar-wrapper">
+        <div className="navbar-left">
+          <h1 className="navbar-logo">BeyondWebs</h1>
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-menu">
+            <li
+              className="navbar-item underline-effect"
+              onClick={handleServicesClick}
+            >
+              Services
+            </li>
+            <li className="navbar-item">
+              <CustomNavLink to={`/projects`}>Projects</CustomNavLink>
+            </li>
+            <li className="navbar-item">
+              <CustomNavLink to={`/about-us`}>About</CustomNavLink>
+            </li>
+            <li className="navbar-item navbar-item-highlight">
+              <CustomNavLink to={`/start-project`}>Start Your Project</CustomNavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Navbar
