5ecc76155f546807c556f7b35af60beae061b2d8
Sufiyan <sufiyanmemon687@gmail.com>
2024-12-25 17:31:17 +0530

    added link and hover arrow effect on project

--- src/pages/Projects/ProjectsWeBuild/ProjectsWeBuild.tsx
+++ src/pages/Projects/ProjectsWeBuild/ProjectsWeBuild.tsx
@@ -4,31 +4,37 @@ const projects = [
     {
         id: 1,
         name: "HiBear",
-        thumbnail: "./images/ProjectsWeBuild/hi-bear.png"
+        thumbnail: "./images/ProjectsWeBuild/hi-bear.png",
+        link: "#"
     },
     {
         id: 2,
         name: "Torch",
-        thumbnail: "./images/ProjectsWeBuild/torch.png"
+        thumbnail: "./images/ProjectsWeBuild/torch.png",
+        link: "#"
     },
     {
         id: 3,
         name: "Raw Cereal",
-        thumbnail: "./images/ProjectsWeBuild/raw-cereal.png"
+        thumbnail: "./images/ProjectsWeBuild/raw-cereal.png",
+        link: "#"
     }, {
         id: 4,
         name: "First Gen",
-        thumbnail: "./images/ProjectsWeBuild/first-gen.png"
+        thumbnail: "./images/ProjectsWeBuild/first-gen.png",
+        link: "#"
     },
     {
         id: 5,
         name: "ZAZA THC",
-        thumbnail: "./images/ProjectsWeBuild/zaza-thc.png"
+        thumbnail: "./images/ProjectsWeBuild/zaza-thc.png",
+        link: "#"
     },
     {
         id: 6,
         name: "Jivati",
-        thumbnail: "./images/ProjectsWeBuild/jivati.png"
+        thumbnail: "./images/ProjectsWeBuild/jivati.png",
+        link: "#"
     }
 ]
 
@@ -38,7 +44,7 @@ const ProjectsWeBuild = () => {
         <div className='projects-we-build-main'>
             { projects.map((project) => (
                 <div key={project.id} className='project-we-build-project'>
-                    <h1>{project.name}</h1>
+                    <h1><a href={project.link}>{project.name}</a></h1>
                     <img src={project.thumbnail}/>
                 </div>
             ))}
