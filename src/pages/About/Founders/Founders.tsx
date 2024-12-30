f601b1e33ad6684acb0684d4978dcaf22ad580c4
Sufiyan <sufiyanmemon687@gmail.com>
2024-12-30 12:54:49 +0530

    added founder page init

---
+++ src/pages/About/Founders/Founders.tsx
@@ -0,0 +1,52 @@
+import "./Founders.css";
+
+const Founders = () => {
+  return (
+    <div className="about-founder-wrapper">
+      <div className="about-founder-main-wrapper">
+        <h5>The founders</h5>
+        <h1>
+          Who's <br /> behind BeyondWebs
+        </h1>
+        <div className="about-founder-info-wrapper">
+          <div>
+            <div>
+              <img />
+            </div>
+            <div>
+              <h1>Derek</h1>
+              <p>
+                Since childhood, I&rsquo;ve had a fascination with computers, coding,
+                and the endless possibilities of technology. That early passion
+                turned into a full-blown obsession, and by the time I was 10, I
+                was already dreaming about creating impactful software
+                solutions. Now, with over 2-3 years of experience as a
+                full-stack developer, I&rsquo;ve transformed that passion into a drive
+                for building applications that are not just functional but
+                exceptionally crafted with perfection in mind.
+              </p>
+              <p>
+                As the founder of BeyondWebs, a service-based startup,
+                I&rsquo;m focused on helping businesses thrive by delivering
+                innovative, scalable, and user-friendly software solutions.
+                Whether it&rsquo;s strategizing a seamless application architecture,
+                diving deep into code, or collaborating with clients to bring
+                their vision to life, I&rsquo;m committed to excellence in every step
+                of the process.
+              </p>
+              <p>
+                When I&rsquo;m not at the keyboard, you might find me brainstorming
+                the next big idea for our clients, leveling up my skills, or
+                dreaming about a future where our company becomes a leading
+                force in the tech industry. At BeyondWebs, we&rsquo;re on a
+                mission to help businesses excel, one line of code at a time.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Founders;
